9c15183a8fc48ab7a08b572cf11c5d67bebbfbf8
Eren <eoksumm@gmail.com>
2024-09-19 16:40:39 +0200

    Bug Fix

--- app/(landing)/_components/Logo.tsx
+++ app/(landing)/_components/Logo.tsx
@@ -24,7 +24,7 @@ export const Logo = () => {
         alt="logo"
         className="hidden dark:block"
       />
-      <p className={cn("font-semibold", font.className)}>Notick</p>
+      <p className={cn("font-semibold", font.className)}>Zotion</p>
     </div>
   );
 };
